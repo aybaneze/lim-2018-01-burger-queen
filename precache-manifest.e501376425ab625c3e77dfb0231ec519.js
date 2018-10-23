4f4a37bc1fb2819ef4487228c2f5b19f2ea1ddaa
aybaneze <andrea270696@gmail.com>
2018-10-23 18:13:52 -0500

    Updates

---
+++ precache-manifest.e501376425ab625c3e77dfb0231ec519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e21b647df43efc1fc099cd5e9871817d",
+    "url": "/lim-2018-01-burger-queen/static/media/img-fond.e21b647d.jpg"
+  },
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "f5d476834ef8fc0211d4",
+    "url": "/lim-2018-01-burger-queen/static/js/main.f5d47683.chunk.js"
+  },
+  {
+    "revision": "3203efd311d763da2705",
+    "url": "/lim-2018-01-burger-queen/static/js/1.3203efd3.chunk.js"
+  },
+  {
+    "revision": "f5d476834ef8fc0211d4",
+    "url": "/lim-2018-01-burger-queen/static/css/main.964d8bc8.chunk.css"
+  },
+  {
+    "revision": "93717da796e918900dfc7337cfdeddca",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
